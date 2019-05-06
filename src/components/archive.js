5482da62fdef1311f302e31ea805df39ee87fb75
gabrilator <gabceboli@gmail.com>
2019-05-07 00:01:38 +0200

    updated archive.js starter

--- src/components/archive.js
+++ src/components/archive.js
@@ -1,11 +1,12 @@
 import React from "react"
-/*import styled from 'styled-components'
+import { StaticQuery, graphql, Link } from "gatsby"
+import styled from 'styled-components'
 const Post = styled.article`
   box-shadow: 0px 3px 10px rgba(25,17,34, 0.05);
   padding: 1rem;
   border-radius: 4px;
   margin-bottom: 1rem;
-`;*/
+`;
 
 // TODO: 
 // 1. Use StaticQuery to query MD information
@@ -13,11 +14,34 @@ const Post = styled.article`
 // 3. Add link to Each blog post
 
 
-
+const POST_ARCHIVE_QUERY =  graphql`
+query BlogInfo {
+allMarkdownRemark (limit: 5, sort:{
+  fields:[frontmatter___date],
+  order:DESC
+}) {
+ edges {
+   node {
+     excerpt(pruneLength:150)
+     frontmatter {
+       title
+       date (formatString:"MMMM DD, YYYY")
+       slug
+     }
+   }
+ }
+}
+}
+` 
 const Archive = () => (
- <div> 
+ <StaticQuery
+ query = {POST_ARCHIVE_QUERY}
+ render= {data=>(
+   <div>
+   
+  </div>
+ )}
  
- 
- </div>
+ />
 )
 export default Archive
