95af994c0b9bf3515bd464e2727b04105f8dea9c
gabrilator <gabceboli@gmail.com>
2019-05-08 02:25:22 +0200

    post deleted

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -3,3 +3,16 @@
 // 2. Add Page query
 // 3. Display title and HTML
     
+import React, { Component } from 'react';
+import Layout from './layout'
+class postLayout extends Component {
+    render() {
+        return (
+            <Layout>
+                
+            </Layout>
+        );
+    }
+}
+
+export default postLayout;
---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Layout from '../components/layout';
+
+class about extends Component {
+    render() {
+        return (
+            <Layout>
+              <h1>About!</h1>   
+              <p>Lorem ipsum dolor amet mustache bitters plaid, squid cred asymmetrical neutra palo santo bushwick semiotics brooklyn raw denim. Jean shorts cold-pressed coloring book humblebrag bushwick echo park banjo bespoke, portland ennui. Squid butcher banjo heirloom tote bag post-ironic cray everyday carry kombucha ethical bitters disrupt gastropub stumptown. Iceland gochujang tumeric venmo, wayfarers truffaut semiotics skateboard humblebrag af. Franzen prism kitsch, distillery green juice pitchfork letterpress austin hammock listicle cornhole waistcoat seitan. Plaid offal edison bulb single-origin coffee gastropub yuccie skateboard pinterest tousled.
+
+              Edison bulb farm-to-ta
+              
+              </p> 
+            </Layout>
+        );
+    }
+}
+
+export default about;
